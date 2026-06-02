296bc599bd1fb975a0c565c5b01666faed046d89
hamysans <liam.d.garcia@gmail.com>
2026-06-02 11:13:30 -0600

    Start FSY Ponder Assignment

---
+++ Ponder/FSY/fsy.js
@@ -0,0 +1,14 @@
+const form = document.querySelector("#fsyForm");
+
+console.log(form);
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    console.log(event)
+
+    const firstName = form.firstName.value;
+    const lastName = form.lastName.value;
+    const email = form.email.value;
+    const campuses = form.campus;
+    console.log(campuses[0].checked)
+})
